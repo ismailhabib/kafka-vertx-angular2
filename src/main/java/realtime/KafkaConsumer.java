23ca1b2be5d1e101cecdfd860c90e54c74095358
Ismail Habib Muhammad <ismail.muhammad@mendix.com>
2015-12-14 15:06:48 +0100

    Initial commit

---
+++ src/main/java/realtime/KafkaConsumer.java
@@ -0,0 +1,37 @@
+package realtime;
+
+import io.vertx.core.AbstractVerticle;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+
+import java.util.Arrays;
+import java.util.Properties;
+
+/**
+ * Created by imu on 12/14/2015.
+ */
+public class KafkaConsumer extends AbstractVerticle {
+
+    @Override
+    public void start() throws Exception {
+
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "ec2-54-165-8-35.compute-1.amazonaws.com:9092");
+        props.put("group.id", "test");
+        props.put("enable.auto.commit", "true");
+        props.put("auto.commit.interval.ms", "1000");
+        props.put("session.timeout.ms", "30000");
+        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        org.apache.kafka.clients.consumer.KafkaConsumer<String, String> consumer = new org.apache.kafka.clients.consumer.KafkaConsumer<String, String>(props);
+        consumer.subscribe(Arrays.asList("test"));
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(100);
+            for (ConsumerRecord<String, String> record : records) {
+                System.out.printf("offset = %d, key = %s, value = %s", record.offset(), record.key(), record.value());
+                vertx.eventBus().publish("news-feed", record.value());
+            }
+        }
+    }
+
+}
